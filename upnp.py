78ad51cef9eacb86d0eeed051bf5ea9a878ecdc1
Petter Helset <petter@helset.eu>
2013-07-10 20:40:32 +0200

    Added UPnP class file

---
+++ upnp.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys, miniupnpc
+
+class UPnP:
+	def __init__(self):
+		self.upnp = miniupnpc.UPnP()
+		self.upnp.discoverdelay = 200;
+
+		# discover upnp igd
+		print 'Discovering UPnP IGDs...'
+		self.upnp.discover()
+
+		# select an igd
+		try:
+			self.upnp.selectigd()
+		except Exception, e:
+			print 'Exception: %s' % e
+			sys.exit(1)
+
+		# display information about the IGD and the internet connection
+		print 'Local IP address: %s' % self.upnp.lanaddr
+		print 'External IP address: %s' % self.upnp.externalipaddress()
+
+	# methods to get mapping info
+
+	def getPortMapping(self, port, protocol):
+		return self.upnp.getspecificportmapping(port, protocol)
+
+	def getFreePort(self, startPort, protocol):
+		while self.getPortMapping(startPort, protocol) != None:
+			return self.getFreePort(startPort + 1, protocol)
+		return startPort
+
+	# methods to check mapping
+
+	def isPortMapped(self, port, protocol):
+		return self.getPortMapping(port, protocol) == None
+
+	def isPortMappedToIp(self, port, protocol, ip):
+		portMapping = self.getPortMapping(port, protocol)
+		if portMapping == None:
+			return False
+		else:
+			(mappedIp, mappedPort, desc, c, d) = portMapping
+			return mappedIp == ip
+
+	# methods to manipulate igd
+
+	def addPortMapping(self, port, protocol, ip):
+		return self.upnp.addportmapping(port, protocol, ip, port, 'miniupnpc python', '')
+
+	def deletePortMapping(self, port, protocol):
+		return self.upnp.deleteportmapping(port, protocol)
+
+	# methods using lanaddr as ip
+
+	def isPortMappedToLanIp(self, port, protocol):
+		return self.isPortMappedToIp(port, protocol, self.upnp.lanaddr)
+
+	def addPortMappingToLanIp(self, port, protocol):
+		return self.addPortMapping(port, protocol, self.upnp.lanaddr)
+
+
+x = UPnP()
+#print x.isPortMapped(14329, 'TCP')
+#print x.isPortMappedToIp(14329, 'TCP', '10.28.21.25')
+#print x.isPortMappedToLanIp(14329, 'TCP')
+#print x.addPortMappingToLanIp(14320, 'TCP')
+#print x.deletePortMapping(14320, 'TCP')
+print x.getFreePort(14329, 'TCP')
